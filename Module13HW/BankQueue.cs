cd2cb06e1c78b117070249c8b699201eba1ddadf
mrdil <mrdil@BMW>
2023-11-26 22:45:16 +0500

    Добавьте файлы проекта.

---
+++ Module13HW/BankQueue.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module13HW
+{
+    class BankQueue
+    {
+        private Queue<Client> queue = new Queue<Client>();
+
+        public void Enqueue(Client client)
+        {
+            queue.Enqueue(client);
+            Console.WriteLine($"{client.Id} встать в очередь на {client.ServiceType}. Текущая длина очереди: {queue.Count}");
+        }
+
+        public void Dequeue()
+        {
+            if (queue.Count > 0)
+            {
+                Client servedClient = queue.Dequeue();
+                Console.WriteLine($"Клиент обслуживается {servedClient.Id} ({servedClient.ServiceType}). Текущая длина очереди: {queue.Count}");
+            }
+            else
+            {
+                Console.WriteLine("Очередь пуста. Нет клиентов, которых нужно обслуживать.");
+            }
+        }
+        public void DisplayQueue()
+        {
+            Console.WriteLine("\nТекущая очередь:");
+            foreach (var client in queue)
+            {
+                Console.WriteLine($"- {client.Id} ({client.ServiceType})");
+            }
+        }
+    }
+}
---
+++ Module13HW/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module13HW
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            
+            BankQueue bankQueue = new BankQueue();
+
+            while (true)
+            {
+                
+                Console.WriteLine("Выберите действие:");
+                Console.WriteLine("1. Добавить клиента в очередь");
+                Console.WriteLine("2. Обслуживайте следующего клиента");
+                Console.WriteLine("3. Показать текущую очередь");
+                Console.WriteLine("0. Выйти из программы");
+
+                string choice = Console.ReadLine();
+
+                
+                
+                switch (choice)
+                {
+                    
+                    
+                    
+                    case "1":
+                        Console.Write("Введите идентификатор клиента: ");
+                        string clientId = Console.ReadLine();
+                        Console.Write("Выберите тип услуги (кредит, депозит, консультация и т.д.): ");
+                        string serviceType = Console.ReadLine();
+                        bankQueue.Enqueue(new Client(clientId, serviceType));
+                        break;
+                    case "2":
+                        bankQueue.Dequeue();
+                        break;
+                    case "3":
+                        bankQueue.DisplayQueue();
+                        break;
+                    case "0":
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Неправильный ввод. Пожалуйста, выберите действие из списка.");
+                        break;
+                }
+            }
+        }
+    }
+}
